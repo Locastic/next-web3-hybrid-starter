179e46bf8e0120d2bd80de93c3e9522bffc94cfe
craxrev <mortadha.ghanmi56@gmail.com>
2024-09-24 12:09:20 +0300

    fix walletconnect dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'encoding');
+
+    return config;
+  }
+};
 
 export default nextConfig;
